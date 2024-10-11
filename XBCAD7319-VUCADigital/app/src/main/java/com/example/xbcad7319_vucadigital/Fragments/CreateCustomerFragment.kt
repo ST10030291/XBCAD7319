6f23b1764b043fa8788aa765f54fc6448ef6d90d
ST10033851 <jereshan.sinan11@gmail.com>
2024-10-11 20:08:48 +0200

    Added Error Dialog

--- XBCAD7319-VUCADigital/app/src/main/java/com/example/xbcad7319_vucadigital/Fragments/CreateCustomerFragment.kt
+++ XBCAD7319-VUCADigital/app/src/main/java/com/example/xbcad7319_vucadigital/Fragments/CreateCustomerFragment.kt
@@ -1,5 +1,6 @@
 package com.example.xbcad7319_vucadigital.Fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -110,11 +111,22 @@ class CreateCustomerFragment : Fragment() {
     private fun validateFields(vararg fields: String): Boolean {
         fields.forEach { field ->
             if (field.isEmpty()) {
-                Toast.makeText(requireContext(), "Please enter all values", Toast.LENGTH_SHORT).show()
+                errorDialog()
                 return true
             }
         }
         return false
+    }
+
+    private fun errorDialog() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Invalid Data")
+            .setMessage("Please enter all values")
+            .setPositiveButton("OK") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .create()
+            .show()
     }
 
     private fun populateFields(customer: CustomerModel, view: View) {
