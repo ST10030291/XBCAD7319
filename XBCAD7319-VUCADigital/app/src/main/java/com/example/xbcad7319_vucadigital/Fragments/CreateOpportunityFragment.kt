01a887e49a879e95658d9fdf083529777d52d3e4
Ditesh <dajeekaushil@gmail.com>
2024-10-16 21:08:01 +0200

    Added Images and drop down options

--- XBCAD7319-VUCADigital/app/src/main/java/com/example/xbcad7319_vucadigital/Fragments/CreateOpportunityFragment.kt
+++ XBCAD7319-VUCADigital/app/src/main/java/com/example/xbcad7319_vucadigital/Fragments/CreateOpportunityFragment.kt
@@ -1,11 +1,17 @@
 package com.example.xbcad7319_vucadigital.Fragments
 
+import android.app.DatePickerDialog
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import androidx.compose.material3.DatePickerDialog
 import com.example.xbcad7319_vucadigital.R
+import java.util.Calendar
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +27,7 @@ class CreateOpportunityFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var date: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +42,7 @@ class CreateOpportunityFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        //date = requireView().findViewById(R.id.dateInput)
         return inflater.inflate(R.layout.fragment_create_opportunity, container, false)
     }
 
@@ -56,5 +64,33 @@ class CreateOpportunityFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+    private fun datePicker(){
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+        val datePickerDialog = DatePickerDialog(
+            // on below line we are passing context.
+            requireContext(),
+            { view, year, monthOfYear, dayOfMonth ->
+                // on below line we are setting
+                // date to our edit text.
+                val dat = (dayOfMonth.toString() + "/" + (monthOfYear + 1) + "/" + year)
+                date.setText(dat)
+            },
+            // on below line we are passing year, month
+            // and day for the selected date in our date picker.
+            year,
+            month,
+            day
+        )
+        datePickerDialog.setOnShowListener{
+            val dateLayout = it as DatePickerDialog
+            val okButton = dateLayout.getButton(DatePickerDialog.BUTTON_POSITIVE)//had to set a colour the themes in android studio wont show my buttons
+            okButton.setTextColor(Color.BLACK)
+        }
+
+        datePickerDialog.show()
     }
 }
